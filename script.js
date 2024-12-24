fc8f2437745545b633d0790dd8e559cff0f81dd5
Sanja_3DArt <zangcaguai@gmail.com>
2024-12-24 14:38:05 +0100

    aaa

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('page-button')
+
+button.addEventListener('click', goWeb)
+
+function goWeb (params) {
+  window.location.href = 'TwitchChatTesting/index.html'
+}
