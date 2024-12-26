af17888b38396490f890f7e37d69c8c7fcaabc23
Sanja_3DArt <zangcaguai@gmail.com>
2024-12-26 20:45:17 +0100

    aa

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const twitchButton = document.getElementById('btn-twitch')
-const portfolioButton = document.getElementById('btn-portfolio')
-
-twitchButton.addEventListener('click', _goChatTwitch)
-portfolioButton.addEventListener('click', _goPortfolio)
-
-function _goChatTwitch () {
-  window.location.href = 'TwitchChatTesting/chat.html'
-}
-
-function _goPortfolio () {
-  window.open(
-    'https://sanja3dart.notion.site/Portfolio-Sanja-3DArt-0e8af653b37d486c9a12d128a760e166'
-  )
-}
-
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 
